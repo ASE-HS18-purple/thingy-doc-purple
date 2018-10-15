0191abd7628597f0abfa290e8c4386ac06fc6fb0
Arbër Kuçi <arber.kuci2@gmail.com>
2018-10-15 07:56:16 +0200

    Implemented logout.

--- frontend/src/app/navigation/navigation.component.ts
+++ frontend/src/app/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Authenticate} from '../authentication/authenticate';
 
 @Component({
   selector: 'app-navigation',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: Authenticate) {
+  }
 
   ngOnInit() {
+  }
+
+  performLogOut() {
+    this.auth.logOut();
+    window.location.replace('');
   }
 
 }
